5ef08308ab933339b6b740be27d6e7c0dc73fc3b
fangyajun <1813306692@qq.com>
2020-05-15 09:57:29 +0800

    fyjmall项目结构初始化创建

---
+++ fyjmall-storage/src/test/java/com/fyj/fyjmall/storage/FyjmallStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyj.fyjmall.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FyjmallStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
