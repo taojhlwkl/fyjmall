3dee4d79fa4d5a14ec2b86d7ef15de8b8940ace5
fangyajun <1813306692@qq.com>
2020-05-15 09:58:29 +0800

    fyjmall项目结构初始化创建

---
+++ fyjmall-order/src/main/java/com/fyj/fyjmall/order/FyjmallOrderApplication.java
@@ -0,0 +1,13 @@
+package com.fyj.fyjmall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyjmallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FyjmallOrderApplication.class, args);
+    }
+
+}
