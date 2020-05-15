de713cafa07394251cb7847327ba54ed978fd0f4
fangyajun <1813306692@qq.com>
2020-05-15 09:57:09 +0800

    fyjmall项目结构初始化创建

---
+++ fyjmall-storage/src/main/java/com/fyj/fyjmall/storage/FyjmallStorageApplication.java
@@ -0,0 +1,13 @@
+package com.fyj.fyjmall.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyjmallStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FyjmallStorageApplication.class, args);
+    }
+
+}
